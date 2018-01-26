27d12743d76f2cd9417940d3c9957189f50fb857
Prasad Gote <gote.prasad@yahoo.in>
2018-01-26 14:49:06 +0530

    General Information

--- app/src/main/java/com/example/admin/rmp/MainActivity.java
+++ app/src/main/java/com/example/admin/rmp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.admin.rmp;
 
+import android.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new GeneralInformation());
+    }
+
+    private void loadFragment(Fragment fragment) {
+        GeneralInformation generalinformation = new GeneralInformation();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.framelayout, generalinformation);
+        transaction.commit();
     }
 }
