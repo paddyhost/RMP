b0913e8096cefcf19b75be2bb0602a852a590169
Prasad Gote <gote.prasad@yahoo.in>
2018-01-28 16:30:56 +0530

    Vaccination, test adviced, mhu test

--- app/src/main/java/com/example/admin/rmp/mhu_test/MhuTest.java
+++ app/src/main/java/com/example/admin/rmp/mhu_test/MhuTest.java
@@ -1,4 +1,4 @@
-package com.example.admin.rmp.previous_records;
+package com.example.admin.rmp.mhu_test;
 
 import android.content.Context;
 import android.net.Uri;
@@ -15,44 +15,43 @@ import android.widget.Button;
 import com.example.admin.rmp.R;
 import com.example.admin.rmp.vaccination_record.VaccinationRecord;
 
-public class PreviousRecords extends Fragment {
+public class MhuTest extends Fragment {
 
-    private Button btn_save_prerecord;
+    private Toolbar mhu_toolbar;
+    private Button btn_save_mhu;
 
-    private Toolbar prerecord_toolbar;
-
-    public PreviousRecords() {
+    public MhuTest() {
         // Required empty public constructor
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootview = inflater.inflate(R.layout.fragment_previous_records, container, false);
-        prerecord_toolbar = (Toolbar) rootview.findViewById(R.id.prerecord_toolbar);
-        ((AppCompatActivity)getActivity()).setSupportActionBar(prerecord_toolbar);
-        prerecord_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        View rootview = inflater.inflate(R.layout.fragment_mhu_test, container, false);
+        mhu_toolbar = (Toolbar) rootview.findViewById(R.id.mhu_toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(mhu_toolbar);
+        mhu_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getActivity().onBackPressed();
             }
         });
-        btn_save_prerecord = (Button) rootview.findViewById(R.id.btn_save_prerecord);
-        btn_save_prerecord.setOnClickListener(new View.OnClickListener() {
+        btn_save_mhu = (Button) rootview.findViewById(R.id.btn_save_mhu);
+        btn_save_mhu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
-                VaccinationRecord vaccinationRecord= new VaccinationRecord();
-                fragmentTransaction.replace(R.id.framelayout,vaccinationRecord).addToBackStack(null).commit();
+                MhuTest mhuTest= new MhuTest();
+                fragmentTransaction.replace(R.id.framelayout,mhuTest).addToBackStack(null).commit();
             }
         });
         return rootview;
     }
-
 }
--- app/src/main/java/com/example/admin/rmp/vaccination_record/VaccinationRecord.java
+++ app/src/main/java/com/example/admin/rmp/vaccination_record/VaccinationRecord.java
@@ -1,11 +1,10 @@
-package com.example.admin.rmp.previous_records;
+package com.example.admin.rmp.vaccination_record;
 
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,46 +12,44 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.admin.rmp.R;
-import com.example.admin.rmp.vaccination_record.VaccinationRecord;
+import com.example.admin.rmp.mhu_test.MhuTest;
 
-public class PreviousRecords extends Fragment {
+public class VaccinationRecord extends Fragment {
 
-    private Button btn_save_prerecord;
+    private Toolbar vaccination_toolbar;
+    private Button btn_save_vaccination;
 
-    private Toolbar prerecord_toolbar;
-
-    public PreviousRecords() {
+    public VaccinationRecord() {
         // Required empty public constructor
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootview = inflater.inflate(R.layout.fragment_previous_records, container, false);
-        prerecord_toolbar = (Toolbar) rootview.findViewById(R.id.prerecord_toolbar);
-        ((AppCompatActivity)getActivity()).setSupportActionBar(prerecord_toolbar);
-        prerecord_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        View rootview = inflater.inflate(R.layout.fragment_vaccination_record, container, false);
+        vaccination_toolbar = (Toolbar) rootview.findViewById(R.id.vaccination_toolbar);
+        vaccination_toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getActivity().onBackPressed();
             }
         });
-        btn_save_prerecord = (Button) rootview.findViewById(R.id.btn_save_prerecord);
-        btn_save_prerecord.setOnClickListener(new View.OnClickListener() {
+        btn_save_vaccination = (Button) rootview.findViewById(R.id.btn_save_vaccination);
+        btn_save_vaccination.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
-                VaccinationRecord vaccinationRecord= new VaccinationRecord();
-                fragmentTransaction.replace(R.id.framelayout,vaccinationRecord).addToBackStack(null).commit();
+                MhuTest mhuTest= new MhuTest();
+                fragmentTransaction.replace(R.id.framelayout,mhuTest).addToBackStack(null).commit();
             }
         });
         return rootview;
     }
-
 }
