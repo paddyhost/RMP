af0898dc8da9972700b63ddd95559e9351fd1403
Prasad Gote <gote.prasad@yahoo.in>
2018-01-28 12:12:50 +0530

    Previous records

--- app/src/main/java/com/example/admin/rmp/medical_condition/MedicalCondition.java
+++ app/src/main/java/com/example/admin/rmp/medical_condition/MedicalCondition.java
@@ -4,18 +4,21 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.admin.rmp.R;
+import com.example.admin.rmp.previous_records.PreviousRecords;
 
 public class MedicalCondition extends Fragment {
 
     Toolbar medical_toolbar;
-
+    private Button btnsavemedical;
     public MedicalCondition() {
         // Required empty public constructor
     }
@@ -36,6 +39,15 @@ public class MedicalCondition extends Fragment {
             @Override
             public void onClick(View v) {
                 getActivity().onBackPressed();
+            }
+        });
+        btnsavemedical = (Button) rootview.findViewById(R.id.btn_save_medical);
+        btnsavemedical.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                PreviousRecords previousRecords= new PreviousRecords();
+                fragmentTransaction.replace(R.id.framelayout,previousRecords).addToBackStack(null).commit();
             }
         });
         return rootview;
